ddc77759bba0af921ec96739a421939214a2b5d6
Chris Thoburn <runspired@gmail.com>
2015-05-06 13:40:37 -0500

    teardown on willDestroy and avoid set if isDestroyed

--- addon/debounced.js
+++ addon/debounced.js
@@ -7,7 +7,8 @@ const {
 
 const {
   debounce,
-  join
+  join,
+  cancel
   } = run;
 
 export default function debouncedProperty() {
@@ -17,20 +18,36 @@ export default function debouncedProperty() {
   var method = args.pop();
 
   var __value = null;
+  var __next = null;
+  var __onDestroy = false;
   var __isNotifying = false;
 
-  var methodFn = function(key) {
-    if (!__isNotifying) {
-      __isNotifying = true;
-      __value = method.call(this);
-      join(this, this.propertyDidChange, key);
-    } else {
-      __isNotifying = false;
+  var methodFn = function(key, value, oldValue) {
+
+    if (!this.get('isDestroyed')) {
+      if (!__isNotifying) {
+        __isNotifying = true;
+        __value = method.call(this, key, value, oldValue);
+        if (!this.get('isDestroying')) {
+          join(this, this.propertyDidChange, key);
+        }
+      } else {
+        __isNotifying = false;
+      }
     }
+
   };
 
-  args.push(function(key) {
-    debounce(this, methodFn, key, rate, false);
+  args.push(function(key, value, oldValue) {
+    if (!__onDestroy) {
+      var _super = this.willDestroy;
+      this.willDestroy = function() {
+        cancel(__next);
+        _super();
+      };
+      __onDestroy = true;
+    }
+    __next = debounce(this, methodFn, key, value, oldValue, rate, false);
     return __value;
   });
   return computed.apply(this, args);
--- addon/throttled.js
+++ addon/throttled.js
@@ -7,7 +7,8 @@ const {
 
 const {
   throttle,
-  next
+  next,
+  cancel
   } = run;
 
 export default function throttledProperty() {
@@ -17,14 +18,28 @@ export default function throttledProperty() {
   var method = args.pop();
 
   var __value = null;
+  var __next = null;
+  var __onDestroy = false;
 
-  var methodFn = function(key) {
-    __value = method.call(this);
-    join(this, this.propertyDidChange, key);
+  var methodFn = function(key, value, oldValue) {
+    if (!this.get('isDestroyed')) {
+      __value = method.call(this, key, value, oldValue);
+      if (!this.get('isDestroying')) {
+        next(this, this.propertyDidChange, key);
+      }
+    }
   };
 
-  args.push(function(key) {
-    throttle(this, methodFn, key, rate);
+  args.push(function(key, value, oldValue) {
+    if (!__onDestroy) {
+      var _super = this.willDestroy;
+      this.willDestroy = function() {
+        cancel(__next);
+        _super();
+      };
+      __onDestroy = true;
+    }
+    __next = throttle(this, methodFn, key, value, oldValue, rate);
     return __value;
   });
   return computed.apply(this, args);
